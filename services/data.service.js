8d1c716ab0c3782070e0fb6a9afca5b81bab0e0d
Savas Vedova <savas@stormkit.io>
2024-12-31 11:02:12 +0300

    Remove auth code and return mock response

--- services/data.service.js
+++ services/data.service.js
@@ -1,60 +1,51 @@
-const mongoose = require('mongoose');
-const dbSchemas = require('./database.schema.service');
-const validations = require('../validators/data.validates');
+const mongoose = require("mongoose");
+const dbSchemas = require("./database.schema.service");
+const validations = require("../validators/data.validates");
 
 // tüm verilerin çekilmesi.
 const list = (req, res) => {
-    // find metodu ile filtresiz veri çekme.
-    dbSchemas.TestSchema.find()
-        .then(items => res.status(200).json({ items }));
+  return res.status(200).json({
+    items: [
+      { message: "Hello World!", timestamp: Date.now() },
+      { message: "How are you?", timestamp: Date.now() },
+    ],
+  });
 };
 
 // id ye göre bir verinin çekilmesi.
 const getById = (req, res) => {
-    const valid = validations.getByIdModelValidateSchema.validate(req.params.id);
-
-    if (valid.error)
-        return res.status(400).json({ message: 'Parameter is not valid.', errors: valid.error.details });
-
-    const id = new mongoose.Types.ObjectId(req.params.id);
-
-    // findOne metodu kullanılır. 
-    // _id mongo db de otomatik oluşan key kolon adıdır.
-    dbSchemas.TestSchema.findOne({ _id: id })
-        .then(item => {
-            if (!item) {
-                return res.status(404).json({ message: "Not found." });
-            } else {
-                return res.status(200).json({ item });
-            }
-        });
+  return res
+    .status(200)
+    .json({ item: { message: "Hello World!", timestamp: Date.now() } });
 };
 
 // yeni veri ekleme
 const add = (req, res) => {
+  const model = ({ name, desc } = req.body);
 
-    const model = { name, desc } = req.body;
+  const valid = validations.addModelValidateSchema.validate(model);
 
-    const valid = validations.addModelValidateSchema.validate(model);
+  if (valid.error)
+    return res
+      .status(400)
+      .json({ message: "Model is not valid.", errors: valid.error.details });
 
-    if (valid.error)
-        return res.status(400).json({ message: 'Model is not valid.', errors: valid.error.details });
+  // hangi koleksiyon ile işlem yapılacaksa
+  // onun modeli kullanılır.
+  let data = new dbSchemas.TestSchema({
+    name: model.name,
+    desc: model.desc,
+  });
 
-    // hangi koleksiyon ile işlem yapılacaksa
-    // onun modeli kullanılır.
-    let data = new dbSchemas.TestSchema({
-        name: model.name,
-        desc: model.desc
-    });
-
-    // save metodu ile insert atılır.
-    data.save()
-        .then(() => res.status(201).json(data._id))
-        .catch(err => res.status(500).json({ message: `Error : ${err}` }));
+  // save metodu ile insert atılır.
+  data
+    .save()
+    .then(() => res.status(201).json(data._id))
+    .catch((err) => res.status(500).json({ message: `Error : ${err}` }));
 };
 
 module.exports = {
-    list,
-    getById,
-    add
-}
+  list,
+  getById,
+  add,
+};
