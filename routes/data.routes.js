afb2cd863bf91669de9be0db34974c3c1e843060
Savas Vedova <savas@stormkit.io>
2024-12-31 11:08:39 +0300

    Remove authentication

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const middleware = require('../middlewares/account.middleware');
-const dataService = require('../services/data.service');
+const middleware = require("../middlewares/account.middleware");
+const dataService = require("../services/data.service");
 
-router.get('/data', middleware.authenticate, dataService.list);
-router.get('/data/:id', middleware.authenticate, dataService.getById);
-router.post('/data', middleware.authenticate, middleware.requireAdmin, dataService.add);
+router.get("/data", dataService.list);
+router.get("/data/:id", dataService.getById);
+router.post(
+  "/data",
+  middleware.authenticate,
+  middleware.requireAdmin,
+  dataService.add
+);
 
 module.exports = router;
